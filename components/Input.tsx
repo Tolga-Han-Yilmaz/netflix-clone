aec47bb9ef958d71472fc8e01b786a4f67544837
Tolga-Han-Yilmaz <101406062+Tolga-Han-Yilmaz@users.noreply.github.com>
2023-04-10 17:51:14 +0300

    auth started

---
+++ components/Input.tsx
@@ -0,0 +1,25 @@
+const Input = () => {
+  return (
+    <div className="relative">
+      <input
+        className="
+      block
+      rounded-md
+      px-6
+      pt-6
+      pb-1
+      w-full
+      text-md
+      text-white
+        bg-neutral-700
+        appearance-none
+        focus:outline-none
+        peer
+        "
+        placeholder=" "
+      />
+    </div>
+  );
+};
+
+export default Input;
