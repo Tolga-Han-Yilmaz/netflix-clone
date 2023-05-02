af50cb490650bb1c45cbc7e9faabbc7510bae10a
Tolga-Han-Yilmaz <101406062+Tolga-Han-Yilmaz@users.noreply.github.com>
2023-05-02 17:34:58 +0300

    Signed in with google and github accounts

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -4,6 +4,9 @@ import Input from "@/components/Input";
 import { signIn } from "next-auth/react";
 import { useRouter } from "next/router";
 
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
+
 const Auth = () => {
   const router = useRouter();
 
@@ -48,8 +51,6 @@ const Auth = () => {
     }
   }, [email, name, password, login]);
 
-  
-
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
       <div className="bg-black w-full h-full lg:bg-opacity-50">
@@ -90,6 +91,40 @@ const Auth = () => {
               className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
               {variant === "login" ? "Sign up" : "Login"}
             </button>
+            <div className="flex flex-row items-center gap-4 mt-8 justify-center">
+              <div
+                onClick={() => signIn("google", { callbackUrl: "/" })}
+                className="
+                      w-10
+                      h-10
+                      bg-white
+                      rounded-full
+                      flex
+                      items-center
+                      justify-center
+                      cursor-pointer
+                      hover:opacity-80
+                      transition      
+                ">
+                <FcGoogle size={30} />
+              </div>
+              <div
+                onClick={() => signIn("github", { callbackUrl: "/" })}
+                className="
+                      w-10
+                      h-10
+                      bg-white
+                      rounded-full
+                      flex
+                      items-center
+                      justify-center
+                      cursor-pointer
+                      hover:opacity-80
+                      transition      
+                ">
+                <FaGithub size={30} />
+              </div>
+            </div>
             <p className="text-neutral-500 mt-12">
               {variant === "login"
                 ? "Already have an account"
