0bbc0b5bfe8a08a6d410a5893ca5c40b4424c207
Tolga-Han-Yilmaz <101406062+Tolga-Han-Yilmaz@users.noreply.github.com>
2023-05-04 12:22:54 +0300

    profiles screen finished

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -27,15 +27,12 @@ const Auth = () => {
       await signIn("credentials", {
         email,
         password,
-        redirect: false,
-        callbackUrl: "/",
+        callbackUrl: "/profiles",
       });
-
-      router.push("/");
     } catch (error) {
       console.log(error);
     }
-  }, [email, password, router]);
+  }, [email, password]);
 
   const register = useCallback(async () => {
     try {
@@ -93,7 +90,7 @@ const Auth = () => {
             </button>
             <div className="flex flex-row items-center gap-4 mt-8 justify-center">
               <div
-                onClick={() => signIn("google", { callbackUrl: "/" })}
+                onClick={() => signIn("google", { callbackUrl: "/profiles" })}
                 className="
                       w-10
                       h-10
@@ -109,7 +106,7 @@ const Auth = () => {
                 <FcGoogle size={30} />
               </div>
               <div
-                onClick={() => signIn("github", { callbackUrl: "/" })}
+                onClick={() => signIn("github", { callbackUrl: "/profiles" })}
                 className="
                       w-10
                       h-10
